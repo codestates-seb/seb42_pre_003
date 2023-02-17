56cf0119d360866b5b847bbd482d30db6764a757
FaberJoo <sjdk0805@gmail.com>
2023-02-17 10:52:34 +0900

    [추가] 프로젝트 생성

---
+++ stackoverflow-be/src/test/java/com/jmc/stackoverflowbe/StackoverflowBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jmc.stackoverflowbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StackoverflowBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
