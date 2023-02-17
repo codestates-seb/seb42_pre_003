bfb4bea85ac5df298520314a4f9f1eb1128f7cde
SSH <school023@gmail.com>
2023-02-17 14:01:55 +0900

    [추가] Member.java

---
+++ stackoverflow-be/src/main/java/com/jmc/stackoverflowbe/member/entity/Member.java
@@ -0,0 +1,22 @@
+package com.jmc.stackoverflowbe.member.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@Getter
+@Setter
+@Entity
+public class Member {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long memberId;
+    @Column
+    private String memberEmail;
+}
