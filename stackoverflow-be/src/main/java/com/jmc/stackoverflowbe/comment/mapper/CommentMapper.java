d6b88d9cc49fa3c709a52ae06ff872207ce33649
JihooKang-KOR <jihoo.kang.kor@gmail.com>
2023-02-20 22:20:20 +0900

    [추가] 댓글 Mapper

--- stackoverflow-be/src/main/java/com/jmc/stackoverflowbe/comment/mapper/CommentMapper.java
+++ stackoverflow-be/src/main/java/com/jmc/stackoverflowbe/comment/mapper/CommentMapper.java
@@ -1,8 +1,15 @@
 package com.jmc.stackoverflowbe.comment.mapper;
 
+import com.jmc.stackoverflowbe.comment.dto.CommentDto;
+import com.jmc.stackoverflowbe.comment.entity.Comment;
 import org.mapstruct.Mapper;
 
 @Mapper(componentModel = "spring")
 public interface CommentMapper {
 
+    Comment PostDtoToComment(CommentDto.Post post);
+
+    Comment PatchDtoToComment(CommentDto.Patch patch);
+
+    CommentDto.Response commentToResponseDto(Comment comment);
 }
