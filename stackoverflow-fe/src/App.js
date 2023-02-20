2b907a7de75cc2418fab4d7d8b69f2ac1f16d941
annkim7 <ilc7748@gmail.com>
2023-02-20 13:46:03 +0900

    [추가] 디렉토리 구성

--- stackoverflow-fe/src/App.js
+++ stackoverflow-fe/src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import GlobalStyle from './style/GlobalStyle';
+import Header from './components/container/Header';
+import Footer from './components/container/Footer';
+import Contents from './page/Contents';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<>
+			<GlobalStyle />
+			<div className='App'>
+				<Header />
+				<Contents />
+				<Footer />
+			</div>
+		</>
+	);
 }
 
 export default App;
---
+++ stackoverflow-fe/src/page/Contents.js
@@ -0,0 +1,5 @@
+function Contents() {
+	return <div>컨텐츠입니다</div>;
+}
+
+export default Contents;
