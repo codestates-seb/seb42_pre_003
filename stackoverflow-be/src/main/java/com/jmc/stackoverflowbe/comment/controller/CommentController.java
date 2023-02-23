c5542d32dc78aed6f024195768c44cecf2f23e93
JihooKang-KOR <jihoo.kang.kor@gmail.com>
2023-02-23 13:42:25 +0900

    [추가] 댓글 Controller에 유효성 관련 annotation들 추가

--- stackoverflow-be/src/main/java/com/jmc/stackoverflowbe/comment/controller/CommentController.java
+++ stackoverflow-be/src/main/java/com/jmc/stackoverflowbe/comment/controller/CommentController.java
@@ -8,9 +8,12 @@ import com.jmc.stackoverflowbe.comment.service.CommentService;
 import com.jmc.stackoverflowbe.global.common.SingleResponseDto;
 import com.jmc.stackoverflowbe.global.utils.UriCreator;
 import java.net.URI;
+import javax.validation.Valid;
+import javax.validation.constraints.Positive;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
@@ -23,6 +26,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/comments")
+@Validated
 @RequiredArgsConstructor
 public class CommentController {
 
@@ -30,9 +34,9 @@ public class CommentController {
     private final CommentMapper mapper;
 
     @PostMapping
-    public ResponseEntity postComment(@RequestBody CommentDto.Post post,
+    public ResponseEntity postComment(@Valid @RequestBody CommentDto.Post post,
         @RequestParam QAState qaState,
-        @RequestParam long qaId) {
+        @Positive @RequestParam long qaId) {
         Comment comment = commentService.createComment(
             mapper.postDtoToComment(post), qaState, qaId);
         URI location = UriCreator.createURI("/comments", comment.getCommentId());
@@ -41,17 +45,17 @@ public class CommentController {
     }
 
     @PatchMapping("/{comment-id}")
-    public ResponseEntity patchComment(@RequestBody CommentDto.Patch patch,
-        @PathVariable("comment-id") long commentId,
+    public ResponseEntity patchComment(@Valid @RequestBody CommentDto.Patch patch,
+        @Positive @PathVariable("comment-id") long commentId,
         @RequestParam QAState qaState,
-        @RequestParam long qaId) {
+        @Positive @RequestParam long qaId) {
         commentService.updateComment(mapper.patchDtoToComment(patch), qaState, qaId);
 
         return ResponseEntity.ok().build();
     }
 
     @GetMapping("/{comment-id}")
-    public ResponseEntity getComment(@PathVariable("comment-id") long commentId) {
+    public ResponseEntity getComment(@Positive @PathVariable("comment-id") long commentId) {
         Comment comment = commentService.getComment(commentId);
 
         return new ResponseEntity<>(new SingleResponseDto<>(
@@ -60,7 +64,7 @@ public class CommentController {
     }
 
     @DeleteMapping("/{comment-id}")
-    public ResponseEntity deleteComment(@PathVariable("comment-id") long commentId) {
+    public ResponseEntity deleteComment(@Positive @PathVariable("comment-id") long commentId) {
         commentService.deleteComment(commentId);
 
         return ResponseEntity.noContent().build();
