f27446ac04ce6563ef2105be2acde20e98c79261
JihooKang-KOR <jihoo.kang.kor@gmail.com>
2023-02-23 20:04:11 +0900

    [추가] OAuth2 관련 설정 및 패키지 추가

--- stackoverflow-be/src/main/java/com/jmc/stackoverflowbe/auth/oauth/config/SecurityConfiguration.java
+++ stackoverflow-be/src/main/java/com/jmc/stackoverflowbe/auth/oauth/config/SecurityConfiguration.java
@@ -1,5 +1,99 @@
 package com.jmc.stackoverflowbe.auth.oauth.config;
 
+import static org.springframework.security.config.Customizer.withDefaults;
+
+import com.jmc.stackoverflowbe.auth.oauth.filter.JwtVerificationFilter;
+import com.jmc.stackoverflowbe.auth.oauth.handler.MemberAccessDeniedHandler;
+import com.jmc.stackoverflowbe.auth.oauth.handler.MemberAuthenticationEntryPoint;
+import com.jmc.stackoverflowbe.auth.oauth.handler.OAuth2MemberSuccessHandler;
+import com.jmc.stackoverflowbe.auth.oauth.jwt.JwtTokenizer;
+import com.jmc.stackoverflowbe.auth.oauth.utils.CustomAuthorityUtils;
+import com.jmc.stackoverflowbe.member.service.MemberService;
+import java.util.Arrays;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.oauth2.client.web.OAuth2LoginAuthenticationFilter;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
 public class SecurityConfiguration {
 
+    private final JwtTokenizer jwtTokenizer;
+    private final CustomAuthorityUtils authorityUtils;
+    private final MemberService memberService;
+
+    public SecurityConfiguration(JwtTokenizer jwtTokenizer,
+        CustomAuthorityUtils authorityUtils,
+        MemberService memberService) {
+        this.jwtTokenizer = jwtTokenizer;
+        this.authorityUtils = authorityUtils;
+        this.memberService = memberService;
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+            .headers().frameOptions().sameOrigin()
+            .and()
+            .csrf().disable()
+            .cors(withDefaults())
+            .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+            .and()
+            .formLogin().disable()
+            .httpBasic().disable()
+            .exceptionHandling()
+            .authenticationEntryPoint(new MemberAuthenticationEntryPoint())
+            .accessDeniedHandler(new MemberAccessDeniedHandler())
+            .and()
+            .apply(new CustomFilterConfigurer())
+            .and()
+            .authorizeHttpRequests(authorize -> authorize
+                .antMatchers(HttpMethod.POST, "/*/members")
+                .permitAll()
+                .antMatchers(HttpMethod.PATCH, "/*/members/**")
+                .hasRole("USER")
+                .antMatchers(HttpMethod.GET, "/*/members/**")
+                .hasAnyRole("ADMIN", "USER")
+                .antMatchers(HttpMethod.DELETE, "/*/members/**")
+                .hasRole("USER")
+                .antMatchers(HttpMethod.POST, "/*/questions").hasRole("USER")
+                .antMatchers(HttpMethod.PATCH, "/*/questions/**").hasAnyRole("USER")
+                .antMatchers(HttpMethod.GET, "/*/questions/**").hasAnyRole("USER", "ADMIN")
+                .antMatchers(HttpMethod.DELETE, "/*/questions/**").hasRole("USER")
+                .anyRequest().permitAll()
+            )
+            .oauth2Login(oauth2 -> oauth2
+                .successHandler(
+                    new OAuth2MemberSuccessHandler(jwtTokenizer, authorityUtils, memberService))
+            );
+
+        return http.build();
+    }
+
+    @Bean
+    CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(Arrays.asList("*"));
+        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PATCH", "DELETE"));
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+    }
+
+    public class CustomFilterConfigurer extends
+        AbstractHttpConfigurer<CustomFilterConfigurer, HttpSecurity> {
+
+        @Override
+        public void configure(HttpSecurity builder) throws Exception {
+            JwtVerificationFilter jwtVerificationFilter =
+                new JwtVerificationFilter(jwtTokenizer, authorityUtils);
+
+            builder.addFilterAfter(jwtVerificationFilter, OAuth2LoginAuthenticationFilter.class);
+        }
+    }
 }
--- stackoverflow-be/src/main/java/com/jmc/stackoverflowbe/auth/oauth/handler/MemberAuthenticationEntryPoint.java
+++ stackoverflow-be/src/main/java/com/jmc/stackoverflowbe/auth/oauth/handler/MemberAuthenticationEntryPoint.java
@@ -1,11 +1,12 @@
-/*
 package com.jmc.stackoverflowbe.auth.oauth.handler;
 
+import com.jmc.stackoverflowbe.auth.oauth.utils.ErrorResponder;
 import java.io.IOException;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.stereotype.Component;
@@ -17,7 +18,14 @@ public class MemberAuthenticationEntryPoint implements AuthenticationEntryPoint 
     @Override
     public void commence(HttpServletRequest request, HttpServletResponse response,
         AuthenticationException authException) throws IOException, ServletException {
+        Exception exception = (Exception) request.getAttribute("exception");
+        ErrorResponder.sendErrorResponse(response, HttpStatus.UNAUTHORIZED);
 
+        logExceptionMessage(authException, exception);
+    }
+
+    private void logExceptionMessage(AuthenticationException authException, Exception exception) {
+        String message = exception != null ? exception.getMessage() : authException.getMessage();
+        log.warn("Unauthorized error happened: {}", message);
     }
 }
-*/
