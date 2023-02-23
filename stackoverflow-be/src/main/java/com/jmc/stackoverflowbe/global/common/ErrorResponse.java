d75aff1c3a2e4bb29c0804150e5724c26457bf52
JihooKang-KOR <jihoo.kang.kor@gmail.com>
2023-02-23 20:03:14 +0900

    [추가] Exception 발생 시 오류 메시지 추가

--- stackoverflow-be/src/main/java/com/jmc/stackoverflowbe/global/common/ErrorResponse.java
+++ stackoverflow-be/src/main/java/com/jmc/stackoverflowbe/global/common/ErrorResponse.java
@@ -1,8 +1,19 @@
 package com.jmc.stackoverflowbe.global.common;
 
 import lombok.Getter;
+import org.springframework.http.HttpStatus;
 
 @Getter
 public class ErrorResponse {
+    private int status;
+    private String message;
 
+    public ErrorResponse(int status, String message) {
+        this.status = status;
+        this.message = message;
+    }
+
+    public static ErrorResponse of(HttpStatus httpStatus) {
+        return new ErrorResponse(httpStatus.value(), httpStatus.getReasonPhrase());
+    }
 }
