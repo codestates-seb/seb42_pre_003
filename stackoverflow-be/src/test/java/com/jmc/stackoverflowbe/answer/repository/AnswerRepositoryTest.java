510454f1f6907b31f80165c3a76adf4a132c672a
FaberJoo <sjdk0805@gmail.com>
2023-02-24 10:41:55 +0900

    [테스트] Answer 테스트

---
+++ stackoverflow-be/src/test/java/com/jmc/stackoverflowbe/answer/repository/AnswerRepositoryTest.java
@@ -0,0 +1,8 @@
+package com.jmc.stackoverflowbe.answer.repository;
+
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+@DataJpaTest
+public class AnswerRepositoryTest {
+
+}
