2d43ca1c937c88bcfb138258eef3e462cc927e3f
FaberJoo <sjdk0805@gmail.com>
2023-02-24 13:04:16 +0900

    [수정] 답변 import 제거

--- stackoverflow-be/src/main/java/com/jmc/stackoverflowbe/answer/mapper/AnswerMapper.java
+++ stackoverflow-be/src/main/java/com/jmc/stackoverflowbe/answer/mapper/AnswerMapper.java
@@ -3,9 +3,6 @@ package com.jmc.stackoverflowbe.answer.mapper;
 import com.jmc.stackoverflowbe.answer.dto.AnswerDto;
 import com.jmc.stackoverflowbe.answer.dto.AnswerDto.AnswerMultiResponseDto;
 import com.jmc.stackoverflowbe.answer.entity.Answer;
-import com.jmc.stackoverflowbe.comment.dto.CommentDto;
-import com.jmc.stackoverflowbe.comment.dto.CommentDto.Response;
-import com.jmc.stackoverflowbe.comment.entity.Comment;
 import java.util.List;
 import org.mapstruct.Mapper;
 
@@ -13,7 +10,10 @@ import org.mapstruct.Mapper;
 public interface AnswerMapper {
 
     Answer postDtoToAnswer(AnswerDto.Post post);
+
     Answer patchDtoToAnswer(AnswerDto.Patch patch);
+
     AnswerDto.Response answerToResponseDto(Answer answer);
+
     List<AnswerMultiResponseDto> answersToResponseDtos(List<Answer> answers);
 }
