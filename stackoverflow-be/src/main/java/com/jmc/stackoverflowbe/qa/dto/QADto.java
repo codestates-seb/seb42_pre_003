ce9d4d9deeb6b0c23599f98c62069c58f8bf4aa6
JihooKang-KOR <jihoo.kang.kor@gmail.com>
2023-02-24 11:02:37 +0900

    Revert "[삭제] 향후 merge를 위해 질답 파트 일괄 삭제"
    This reverts commit 2636726558b3bcd7a38ce7d8664cd9daf50b6a06.

---
+++ stackoverflow-be/src/main/java/com/jmc/stackoverflowbe/qa/dto/QADto.java
@@ -0,0 +1,87 @@
+package com.jmc.stackoverflowbe.qa.dto;
+
+import com.jmc.stackoverflowbe.article.entity.Article;
+import com.jmc.stackoverflowbe.global.audit.Auditable;
+import com.jmc.stackoverflowbe.member.entity.Member;
+import com.jmc.stackoverflowbe.qa.entity.QA.QaGroup;
+import com.jmc.stackoverflowbe.qa.entity.QA.StateGroup;
+import java.sql.Timestamp;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+public class QADto {
+    @Getter
+    @NoArgsConstructor
+    public static class Post {
+        private Long articleId;
+        private Long memberId;
+        private String qaContent;
+
+        @Builder
+        public Post(Long articleId, Long memberId, String qaContent) {
+            this.articleId = articleId;
+            this.memberId = memberId;
+            this.qaContent = qaContent;
+        }
+    }
+
+    @Getter
+    @NoArgsConstructor
+    public static class Patch {
+
+        private Long qaId;
+        private String qaContent;
+        private Integer votes;
+        @Builder
+        public Patch(Long qaId, String qaContent, Integer votes) {
+            this.qaId = qaId;
+            this.qaContent = qaContent;
+            this.votes = votes;
+        }
+    }
+
+    @Getter
+    @NoArgsConstructor
+    public static class Response {
+
+        private Long id;
+        //private Article article;
+        //private Member member;
+        private Long articleId;
+        private Long memberId;
+        private String qaContent;
+        private Integer votes;
+        private QaGroup group;
+        private StateGroup state;
+
+        private Timestamp createdAt;
+
+        private Timestamp modifiedAt;
+
+//        @Builder
+//        public Response(Long id, Article article, Member member, String qaContent, Integer votes,
+//            QaGroup group, StateGroup state) {
+//            this.id = id;
+//            this.article = article;
+//            this.member = member;
+//            this.qaContent = qaContent;
+//            this.votes = votes;
+//            this.group = group;
+//            this.state = state;
+//        }
+
+        @Builder
+        public Response(Long id, Long articleId, Long memberId, String qaContent, Integer votes,
+            QaGroup group, StateGroup state) {
+            this.id = id;
+            this.articleId = articleId;
+            this.memberId = memberId;
+            this.qaContent = qaContent;
+            this.votes = votes;
+            this.group = group;
+            this.state = state;
+        }
+    }
+}
---
+++ stackoverflow-be/src/test/java/com/jmc/stackoverflowbe/qa/controller/QAControllerTest.java
@@ -0,0 +1,258 @@
+package com.jmc.stackoverflowbe.qa.controller;
+
+
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.startsWith;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.doNothing;
+import static org.springframework.restdocs.headers.HeaderDocumentation.headerWithName;
+import static org.springframework.restdocs.headers.HeaderDocumentation.responseHeaders;
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
+import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.delete;
+import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.post;
+import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.patch;
+import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.get;
+import static org.springframework.restdocs.operation.preprocess.Preprocessors.preprocessRequest;
+import static org.springframework.restdocs.operation.preprocess.Preprocessors.preprocessResponse;
+import static org.springframework.restdocs.operation.preprocess.Preprocessors.prettyPrint;
+import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
+import static org.springframework.restdocs.payload.PayloadDocumentation.requestFields;
+import static org.springframework.restdocs.payload.PayloadDocumentation.responseFields;
+import static org.springframework.restdocs.request.RequestDocumentation.parameterWithName;
+import static org.springframework.restdocs.request.RequestDocumentation.pathParameters;
+import static org.springframework.restdocs.snippet.Attributes.attributes;
+import static org.springframework.restdocs.snippet.Attributes.key;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.header;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import com.google.gson.Gson;
+import com.jmc.stackoverflowbe.article.entity.Article;
+import com.jmc.stackoverflowbe.member.entity.Member;
+import com.jmc.stackoverflowbe.qa.dto.QADto;
+import com.jmc.stackoverflowbe.qa.entity.QA;
+import com.jmc.stackoverflowbe.qa.entity.QA.QaGroup;
+import com.jmc.stackoverflowbe.qa.entity.QA.StateGroup;
+import com.jmc.stackoverflowbe.qa.mapper.QAMapper;
+import com.jmc.stackoverflowbe.qa.service.QAService;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.data.jpa.mapping.JpaMetamodelMappingContext;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.restdocs.payload.JsonFieldType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+
+@WebMvcTest(QAController.class)
+@MockBean(JpaMetamodelMappingContext.class)
+@AutoConfigureRestDocs
+public class QAControllerTest {
+    String BASE_URL = "/qas";
+
+    QA qa = QA.builder()
+        .id(1L)
+        .qaContent("testing content")
+        .qaGroup(QaGroup.QUESTION)
+        .state(StateGroup.ACTIVE)
+        .votes(0L)
+        .memberId(0L)
+        .articleId(0L)
+        .build();
+
+    @Autowired
+    MockMvc mockMvc;
+
+    @MockBean
+    QAService qaService;
+
+    @MockBean
+    QAMapper mapper;
+
+    @Autowired
+    Gson gson;
+
+    @DisplayName("Qa 생성")
+    @Test
+    void postQATest() throws Exception{
+        QADto.Post post = QADto.Post.builder()
+            .articleId(0L)
+            .memberId(0L)
+            .qaContent("post testing content")
+            .build();
+
+        String content = gson.toJson(post);
+        given(mapper.PostDtoToQA(Mockito.any(QADto.Post.class)))
+            .willReturn(qa);
+        given(qaService.createQA(Mockito.any(QA.class)))
+            .willReturn(qa);
+
+        ResultActions actions = mockMvc.perform(
+            post(BASE_URL)
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON)
+                .content(content));
+
+        actions
+            .andExpect(status().isCreated())
+            .andExpect(header().string("Location", is(startsWith("/qas/"))))
+            .andDo(document("Post-QA",
+                preprocessRequest(prettyPrint()),
+                preprocessResponse(prettyPrint()),
+                requestFields(
+                    attributes(key("title").value("Fields for qa creation")),
+                    fieldWithPath("articleId")
+                        .type(JsonFieldType.NUMBER)
+                        .attributes(key("constraints").value("글"))
+                        .description("작성된 글"),
+                    fieldWithPath("memberId")
+                        .type(JsonFieldType.NUMBER)
+                        .attributes(key("constraints").value("작성자"))
+                        .description("질답 작성자"),
+                    fieldWithPath("qaContent")
+                        .type(JsonFieldType.STRING)
+                        .attributes(key("constraints").value("내용"))
+                        .description("질답 내용")),
+                responseHeaders(
+                    headerWithName(HttpHeaders.LOCATION)
+                        .description("Header Location, 리소스의 URL")
+                )));
+    }
+
+    @DisplayName("질답 수정")
+    @Test
+    void patchQATest() throws Exception{
+        QADto.Patch patch = QADto.Patch.builder()
+            .qaId(1L)
+            .qaContent("patch testing content")
+            .votes(0)
+            .build();
+
+        String content = gson.toJson(patch);
+        given(mapper.PatchDtoToQA(Mockito.any(QADto.Patch.class)))
+            .willReturn(new QA());
+        given(qaService.updateQA(Mockito.any(QA.class)))
+            .willReturn(qa);
+
+        ResultActions actions = mockMvc.perform(
+            patch(BASE_URL+"/{qa-id}", patch.getQaId())
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON)
+                .content(content));
+
+        actions
+            .andExpect(status().isOk())
+            .andDo(document("Patch-QA",
+                preprocessRequest(prettyPrint()),
+                preprocessResponse(prettyPrint()),
+                requestFields(
+                    attributes(key("title").value("Fields for qa revision")),
+                    fieldWithPath("qaId")
+                        .type(JsonFieldType.NUMBER)
+                        .attributes(key("constraints").value("질답 식별자"))
+                        .description("질답 식별자"),
+                    fieldWithPath("qaContent")
+                        .type(JsonFieldType.STRING)
+                        .attributes(key("constraints").value("내용"))
+                        .description("수정한 내용"),
+                    fieldWithPath("votes")
+                        .type(JsonFieldType.NUMBER)
+                        .attributes(key("constraints").value("투표"))
+                        .description("투표의 증감"))));
+    }
+
+    @DisplayName("질답 조회")
+    @Test
+    void getQATest() throws Exception{
+        QADto.Response response = QADto.Response.builder()
+            .id(qa.getId())
+            .articleId(0L)
+            .memberId(0L)
+            .qaContent("get testing content")
+            .votes(0)
+            .group(QaGroup.QUESTION)
+            .state(StateGroup.ACTIVE)
+            .build();
+
+        given(qaService.getQA(Mockito.anyLong()))
+            .willReturn(new QA());
+        given(mapper.QAToResponseDto(Mockito.any(QA.class)))
+            .willReturn(response);
+
+        ResultActions actions = mockMvc.perform(
+            get(BASE_URL+"/{qa-id}", qa.getId())
+                .accept(MediaType.APPLICATION_JSON));
+
+        actions
+            .andExpect(status().isOk())
+            .andExpect(jsonPath("$.data").exists())
+            .andExpect(jsonPath("$.data.articleId").exists())
+            .andExpect(jsonPath("$.data.memberId").exists())
+            .andExpect(jsonPath("$.data.qaContent").exists())
+            .andExpect(jsonPath("$.data.votes").exists())
+            .andExpect(jsonPath("$.data.group").exists())
+            .andExpect(jsonPath("$.data.state").exists())
+            .andDo(document("Get-QA",
+                preprocessResponse(prettyPrint()),
+                pathParameters(
+                    parameterWithName("qa-id")
+                        .description("질답 식별자")),
+                responseFields(
+                    fieldWithPath("data")
+                        .type(JsonFieldType.OBJECT)
+                        .description("조회 데이터"),
+                    fieldWithPath("data.id")
+                        .type(JsonFieldType.NUMBER)
+                        .description("질답 식별자"),
+                    fieldWithPath("data.articleId")
+                        .type(JsonFieldType.NUMBER)
+                        .description("글"),
+                    fieldWithPath("data.memberId")
+                        .type(JsonFieldType.NUMBER)
+                        .description("질답 작성자"),
+                    fieldWithPath("data.qaContent")
+                        .type(JsonFieldType.STRING)
+                        .description("질답 내용"),
+                    fieldWithPath("data.votes")
+                        .type(JsonFieldType.NUMBER)
+                        .description("받은 투표 현황"),
+                    fieldWithPath("data.group")
+                        .type(JsonFieldType.STRING)
+                        .description("질답 구분"),
+                    fieldWithPath("data.state")
+                        .type(JsonFieldType.STRING)
+                        .description("질답 상태"),
+                    fieldWithPath("data.createdAt")
+                        .type(JsonFieldType.NULL)
+                        .description("질답 생성 시간"),
+                    fieldWithPath("data.modifiedAt")
+                        .type(JsonFieldType.NULL)
+                        .description("질답 수정 시간"))));
+
+    }
+
+    @DisplayName("질답 삭제")
+    @Test
+    void deleteQATest() throws Exception{
+        doNothing().when(qaService).deleteQA(qa.getId());
+
+        ResultActions actions = mockMvc.perform(
+            delete(BASE_URL + "/{qa-id}", qa.getId())
+                .accept(MediaType.APPLICATION_JSON));
+
+        actions
+            .andExpect(status().isNoContent())
+            .andExpect(jsonPath("$.data").doesNotExist())
+            .andDo(document("Delete-QA",
+                pathParameters(
+                    parameterWithName("qa-id").description("질답 아이디")
+                ))
+            );
+    }
+
+}
