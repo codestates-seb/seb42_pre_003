abadbfe445b2aa5405a413531c0324624cd48cf2
FaberJoo <sjdk0805@gmail.com>
2023-02-24 10:54:39 +0900

    [수정] 충돌

--- stackoverflow-be/src/main/java/com/jmc/stackoverflowbe/answer/dto/AnswerDto.java
+++ stackoverflow-be/src/main/java/com/jmc/stackoverflowbe/answer/dto/AnswerDto.java
@@ -1,9 +1,7 @@
 package com.jmc.stackoverflowbe.answer.dto;
 
+import com.jmc.stackoverflowbe.answer.entity.Answer;
 import com.jmc.stackoverflowbe.global.audit.Auditable;
-import com.jmc.stackoverflowbe.qa.entity.QA.QaGroup;
-import com.jmc.stackoverflowbe.qa.entity.QA.StateGroup;
-import java.sql.Timestamp;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -29,6 +27,7 @@ public class AnswerDto {
     public static class Patch {
 
         private String answerContent;
+
         @Builder
         public Patch(String answerContent) {
             this.answerContent = answerContent;
@@ -40,30 +39,30 @@ public class AnswerDto {
     public static class Response extends Auditable {
 
         private Long answerId;
-        //private Question Question;
-        //private Member member;
+        // private Question Question;
+        // private Member member;
         private Long questionId;
         private Long memberId;
         private String answerContent;
         private Integer votes;
-        private StateGroup state;
+        private Answer.StateGroup state;
 
-//        @Builder
-//        public Response(Long id, Article article, Member member, String qaContent, Integer votes,
-//            QaGroup group, StateGroup state) {
-//            this.id = id;
-//            this.article = article;
-//            this.member = member;
-//            this.qaContent = qaContent;
-//            this.votes = votes;
-//            this.group = group;
-//            this.state = state;
-//        }
+        // @Builder
+        // public Response(Long id, Article article, Member member, String qaContent,
+        // Integer votes,
+        // QaGroup group, StateGroup state) {
+        // this.id = id;
+        // this.article = article;
+        // this.member = member;
+        // this.qaContent = qaContent;
+        // this.votes = votes;
+        // this.group = group;
+        // this.state = state;
+        // }
 
         @Builder
         public Response(Long answerId, Long questionId, Long memberId, String answerContent,
-            Integer votes,
-            StateGroup state) {
+                Integer votes, Answer.StateGroup state) {
             this.answerId = answerId;
             this.questionId = questionId;
             this.memberId = memberId;
--- stackoverflow-be/src/test/java/com/jmc/stackoverflowbe/question/service/QuestionServiceTest.java
+++ stackoverflow-be/src/test/java/com/jmc/stackoverflowbe/question/service/QuestionServiceTest.java
@@ -4,10 +4,6 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 @ExtendWith(MockitoExtension.class)
-<<<<<<<< HEAD:stackoverflow-be/src/test/java/com/jmc/stackoverflowbe/question/service/AnswerServiceTest.java
-public class AnswerServiceTest {
-========
 public class QuestionServiceTest {
->>>>>>>> eda0151448092f3ae1becbfa09bb34a48a456f34:stackoverflow-be/src/test/java/com/jmc/stackoverflowbe/question/service/QuestionServiceTest.java
-    
+
 }
