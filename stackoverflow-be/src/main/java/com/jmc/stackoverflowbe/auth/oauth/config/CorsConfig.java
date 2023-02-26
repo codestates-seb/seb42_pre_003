30640469a378fea16c92becc6f1ba77e3a0e988d
FaberJoo <sjdk0805@gmail.com>
2023-02-26 20:32:32 +0900

    [리팩토링] CorsConfg  분리

---
+++ stackoverflow-be/src/main/java/com/jmc/stackoverflowbe/auth/oauth/config/CorsConfig.java
@@ -0,0 +1,24 @@
+package com.jmc.stackoverflowbe.auth.oauth.config;
+
+import java.util.Arrays;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+@Configuration
+@RequiredArgsConstructor
+public class CorsConfig {
+    @Bean
+    CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowedOrigins(Arrays.asList("http://localhost:3000", "http://localhost:80", "http://localhost:8080"));
+        config.setAllowedMethods(Arrays.asList("GET", "POST", "PATCH", "DELETE"));
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        config.setAllowCredentials(true);
+        source.registerCorsConfiguration("/**", config);
+        return source;
+    }
+}
