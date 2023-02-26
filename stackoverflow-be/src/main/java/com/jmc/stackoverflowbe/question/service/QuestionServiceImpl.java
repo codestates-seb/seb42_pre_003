9c305cc2c20627875a4d860a901f52b2feac7a3d
SSH <school023@gmail.com>
2023-02-27 04:06:57 +0900

    [추가] QuestionServiceImpl 주석 추가

--- stackoverflow-be/src/main/java/com/jmc/stackoverflowbe/question/service/QuestionServiceImpl.java
+++ stackoverflow-be/src/main/java/com/jmc/stackoverflowbe/question/service/QuestionServiceImpl.java
@@ -24,58 +24,64 @@ public class QuestionServiceImpl implements QuestionService{
     private final QuestionRepository questionRepository;
     private final QuestionMapper mapper;
 
+    //질문 생성
     @Override
     public Question createQuestion(Question question){
+        //질문 아이디로 검증이 필요할시 추가
         //verifyExistQuestion(question.getQuestionId());
+        //질문 db에 저장
         return questionRepository.save(question);
     }
 
-
+    //질문 수정
     @Override
     public Question updateQuestion(Question question){
+        //db에 저장된 질문인지 확인
         Question findQuestion = findExistQuestion(question.getQuestionId());
+        //수정할 내용이 있으면 수정 아니면 그대로
         Optional.ofNullable(question.getQuestionTitle())
-            .isPresent(title -> findQuestion.setQuestionTitle(title));
-
+            .ifPresent(title -> findQuestion.setQuestionTitle(title));
+        Optional.ofNullable(question.getQuestionContent())
+            .ifPresent(content -> findQuestion.setQuestionContent(content));
+        //변경 사항 db에 적용
         return questionRepository.save(findQuestion);
     }
+    //질문 조회
     @Override
     public Question getQuestion(Long questionId){
-        return Question.builder()
-            .questionId(0L)
-            .questionTitle("Question title for stub")
-            .memberId(0L)
-            .questionContent("Question contents for stub")
-            .state(StateGroup.ACTIVE)
-            .votes(0)
-            .selection(false)
-            .answers(0L)
-            .views(0L)
-            .build();
+        //Id로 질문 조회
+        return findExistQuestion(questionId);
     }
 
+    //질문 전체 조회
     @Override
     public Page<Question> getQuestions(int page, String sort){
+        //전체를 찾아서 페이지네이션 적용 후 반환
         return questionRepository.findAll(
             PageRequest.of(page, 15, Sort.by(sort).descending()));
     }
-
-
+    //질문 삭제
     @Override
     public void deleteQuestion(Long questionId){
-
+        //db에 존재하는 질문인지 확인
+        Question question = findExistQuestion(questionId);
+        //있으면 삭제
+        questionRepository.delete(question);
     }
 
     //질문이 존재하는지 확인
     @Override
     public Question findExistQuestion(Long questionId){
+        //질문id로 존재하는지 확인
         Optional<Question> optionalQuestion = questionRepository.findById(questionId);
+        //질문이 존재하지 않으면 예외 처리
         Question findQuestion =
             optionalQuestion.orElseThrow(()->
                 new BusinessLogicException(ExceptionCode.QUESTION_NOT_FOUND));
+        //존재하면 해당 질문 반환
         return findQuestion;
     }
-
+// 질문 식별자로 존재하는지 확인하는 로직 필요할시 만들어 씀
 //    private void verifyExistQuestion(long questionId){
 //        Optional<Question> question = questionRepository.findById(questionId);
 //        if(question.isPresent())
