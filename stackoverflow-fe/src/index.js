ab058793e6a12861e35b35c20b162d7d66c3af1d
journey0321 <yjyjh0321@gmail.com>
2023-02-27 14:02:14 +0900

    react-query 충돌해결

--- stackoverflow-fe/src/index.js
+++ stackoverflow-fe/src/index.js
@@ -3,9 +3,6 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { BrowserRouter } from 'react-router-dom';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
