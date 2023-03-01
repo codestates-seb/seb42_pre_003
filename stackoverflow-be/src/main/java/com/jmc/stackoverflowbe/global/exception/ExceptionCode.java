0dd79fbe18c667af8ee7c18660a56cf8d73573cb
FaberJoo <sjdk0805@gmail.com>
2023-03-01 14:39:39 +0900

    [수정] 예외 메시지 수정

--- stackoverflow-be/src/main/java/com/jmc/stackoverflowbe/global/exception/ExceptionCode.java
+++ stackoverflow-be/src/main/java/com/jmc/stackoverflowbe/global/exception/ExceptionCode.java
@@ -4,12 +4,12 @@ import lombok.Getter;
 
 public enum ExceptionCode {
     MEMBER_NOT_FOUND(404, "Member Not Found"),
-    MEMBER_NOT_UNAUTHORIZED(401, "Member not Unauthorized"),
+    MEMBER_UNAUTHORIZED(401, "Member Unauthorized"),
     MEMBER_NOT_MATCH(403, "Member Not Match"),
     MEMBER_EMAIL_EXISTS(409, "Email Already Exist."),
     MEMBER_NAME_EXISTS(409, "Name Already Exist."),
     QUESTION_NOT_FOUND(404, "Question Not Found"),
-    QUESTION_EXISTS(409, "Question is exists"),
+    QUESTION_EXISTS(409, "Question exists"),
     ANSWER_NOT_FOUND(404, "Answer Not Found"),
     COMMENT_NOT_FOUND(404, "Comment Not Found");
 
