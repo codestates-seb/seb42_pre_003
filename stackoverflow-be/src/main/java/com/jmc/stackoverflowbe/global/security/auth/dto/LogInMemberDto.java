6a20478882e0fc057f827afb6daa48e94146355e
FaberJoo <sjdk0805@gmail.com>
2023-03-01 12:49:30 +0900

    [수정] Oauth2MemberDto.java -> LoginMemberDto.java

--- stackoverflow-be/src/main/java/com/jmc/stackoverflowbe/global/security/auth/dto/LogInMemberDto.java
+++ stackoverflow-be/src/main/java/com/jmc/stackoverflowbe/global/security/auth/dto/LogInMemberDto.java
@@ -8,12 +8,12 @@ import lombok.Setter;
 @Getter
 @Setter
 @NoArgsConstructor
-public class Oauth2MemberDto {
+public class LogInMemberDto {
     private Long memberId;
     private String email;
 
     @Builder
-    public Oauth2MemberDto(Long memberId, String email) {
+    public LogInMemberDto(Long memberId, String email) {
         this.memberId = memberId;
         this.email = email;
     }
