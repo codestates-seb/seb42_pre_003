7aef823a7998c6adbdd648838de28e35a9f00d49
JihooKang-KOR <jihoo.kang.kor@gmail.com>
2023-03-01 14:48:04 +0900

    Revert "[추가] 예외 코드 MEMBER_NOT_AUTHORIZED 추가"
    This reverts commit cd13a47245f02bc36270d5436fcfe4bc36e6c14a.

--- stackoverflow-be/src/main/java/com/jmc/stackoverflowbe/global/exception/ExceptionCode.java
+++ stackoverflow-be/src/main/java/com/jmc/stackoverflowbe/global/exception/ExceptionCode.java
@@ -6,7 +6,6 @@ public enum ExceptionCode {
     MEMBER_NOT_FOUND(404, "Member not found"),
     MEMBER_EMAIL_EXISTS(409, "Email Already Exist."),
     MEMBER_NAME_EXISTS(409, "Name Already Exist."),
-    MEMBER_NOT_AUTHORIZED(401, "Unauthorized member"),
     QUESTION_NOT_FOUND(404, "Question not found"),
     ANSWER_NOT_FOUND(404, "Answer not found"),
     COMMENT_NOT_FOUND(404, "Comment not found"),
