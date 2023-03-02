89f8f6de86ba2871a0b79a629778339e2fa833ee
FaberJoo <sjdk0805@gmail.com>
2023-03-03 00:13:57 +0900

    [수정] 오탈자 수정

--- stackoverflow-be/src/main/java/com/jmc/stackoverflowbe/global/security/auth/config/CorsConfig.java
+++ stackoverflow-be/src/main/java/com/jmc/stackoverflowbe/global/security/auth/config/CorsConfig.java
@@ -14,7 +14,7 @@ public class CorsConfig {
     @Bean
     CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(Arrays.asList("http://localhost:3000", "http://http://seb42-pre-003.s3-website.ap-northeast-2.amazonaws.com/"));
+        config.setAllowedOrigins(Arrays.asList("http://localhost:3000", "http://seb42-pre-003.s3-website.ap-northeast-2.amazonaws.com/"));
         config.setAllowedMethods(Arrays.asList("GET", "POST", "PATCH", "DELETE"));
 //        config.addAllowedOriginPattern(Arrays.asList("http://localhost:3000", "http://seb42-pre-003.s3-website.ap-northeast-2.amazonaws.com/"));
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
